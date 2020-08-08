302089a72b80a45e730162453fb16630c2d58bfd
Ierofantis <evil69669@gmail.com>
2020-08-08 14:33:51 +0300

    faas service completed

---
+++ handler.js
@@ -0,0 +1,11 @@
+"use strict"
+
+module.exports = (context, callback) => {
+    const content = JSON.parse(context);
+
+    let weather = { 'rainy': 3, 'cloudy': 2, 'snowy': 1, 'sunny': -1, 'other': 0 };
+
+    let index = (1024 / content.airlineId) - (30 * weather[content.condition]);
+
+    callback(undefined, { status: index });
+}
