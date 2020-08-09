ab5fd15e7197b1d8a75625cd15fdac20ef72daf8
Ierofantis <evil69669@gmail.com>
2020-08-09 17:47:04 +0300

    Add Node12

--- handler.js
+++ handler.js
@@ -1,11 +1,21 @@
 "use strict"
 
-module.exports = (context, callback) => {
-    const content = JSON.parse(context);
+module.exports = async (event, context) => {
+    const content = event;
 
     let weather = { 'rainy': 3, 'cloudy': 2, 'snowy': 1, 'sunny': -1, 'other': 0 };
 
-    let index = (1024 / content.airlineRanking) - (30 * weather[content.condition]);
-
-    callback(undefined, { status: index });
-}
+    let index = (1024 / content.body.airlineRanking) - (30 * weather[content.body.condition]);
+    const result = {
+        'status': 'Received input: ' + JSON.stringify(index)
+    }
+    return context
+        .headers(
+            {
+                "Content-type": "text/plain",
+                "Access-Control-Allow-Origin": "*"
+            }
+        )
+        .status(200)
+        .succeed(result)
+} 
